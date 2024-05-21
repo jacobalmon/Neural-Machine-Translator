a95f8a8ad1dc36eeb6abca152e8d3ac55ab5ea42
Jacob Almon <118553065+jacobalmon@users.noreply.github.com>
2024-05-21 02:01:12 -0700

    Update

--- model.py
+++ model.py
@@ -3,4 +3,21 @@ import torch.nn as nn
 class Encoder(nn.Module):
     def __init__(self, input_dim, embedded_dim, hidden_dim, num_layers, dropout):
         super().__init__()
-        pass
+        
+        # Initialize Embedded Layer.
+        self.embedded_layer = nn.Embedding(input_dim, embedded_dim)
+
+        # Initialize LSTM.
+        self.rnn = nn.LSTM(embedded_dim, hidden_dim, num_layers, dropout=dropout)
+
+        # Initialize Dropout Layer.
+        self.dropout= nn.Dropout(dropout)
+
+    def forward(self, src):
+        # Embedding Input Tokens
+        embedded = self.dropout(self.embedded_layer(src))
+
+        # Embedded Input -> LSTM.
+        outputs, (hidden, cell) = self.rnn(embedded)
+
+        return hidden, cell
